9c9221198328845641b45c43255e2d9d3ad08412
Paweł Musiał <pawel.jan.musial@gmail.com>
2012-12-11 18:55:05 +0100

    little fixes

--- src/com/android/ColorBlobMatcher.java
+++ src/com/android/ColorBlobMatcher.java
@@ -36,7 +36,7 @@ public class ColorBlobMatcher {
     Mat mMask = new Mat();
     Mat mDilatedMask = new Mat();
     Mat mHierarchy = new Mat();
-	public static  double SHAPE_DIFF_THRESHOLD = 0.18d;
+	public static  double SHAPE_DIFF_THRESHOLD = 0.15d;
 	public static double DESC_DIFF_THRESHOLD = (3.0d*DescriptorDataset.NORMALIZATION_MAX*(double)DescriptorHandler.HISTOGRAM_BINS)*0.03d;
 	
 
